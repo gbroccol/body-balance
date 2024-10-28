dd2ba93475dd3bb9a254334ba5f98d248dfc3b4d
pchelina.a2 <pchelina.a2@wildberries.work>
2024-10-28 20:32:16 +0100

    to test

--- app/src/main/java/org/body/balance/Application.java
+++ app/src/main/java/org/body/balance/Application.java
@@ -7,5 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        System.out.println("Для тестирования:");
+        System.out.println("http://localhost:8080");
     }
 }
