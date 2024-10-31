edf41ea133fb35ecb5d8f6d0940bd9925c78d6b6
pchelina.a2 <pchelina.a2@wildberries.work>
2024-10-31 23:15:09 +0100

    fix

--- auth/src/main/java/org/body/balance/auth/userdetails/UserDetailsServiceImpl.java
+++ auth/src/main/java/org/body/balance/auth/userdetails/UserDetailsServiceImpl.java
@@ -1,18 +1,18 @@
 package org.body.balance.auth.userdetails;
 
+import lombok.AllArgsConstructor;
 import org.body.balance.auth.domain.AuthUser;
 import org.body.balance.auth.repository.AuthUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    @Autowired
-    private AuthUserRepository authUserRepository;
+    private final AuthUserRepository authUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
