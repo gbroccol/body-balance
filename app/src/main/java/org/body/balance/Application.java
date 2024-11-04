16fe8375d235bdbb29d92917d93105a86b59a92b
pchelina.a2 <pchelina.a2@wildberries.work>
2024-11-04 17:59:14 +0100

    start info msg

--- app/src/main/java/org/body/balance/Application.java
+++ app/src/main/java/org/body/balance/Application.java
@@ -11,6 +11,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        log.info("Для тестирования: http://localhost:8080");
     }
 }
