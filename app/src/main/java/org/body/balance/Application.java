96db523dc260d787a4b65c904c478067f839f8da
pchelina.a2 <pchelina.a2@wildberries.work>
2024-11-06 10:56:07 +0100

    Slf4j

--- app/src/main/java/org/body/balance/Application.java
+++ app/src/main/java/org/body/balance/Application.java
@@ -1,11 +1,11 @@
 package org.body.balance;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
+@Log4j2
 @SpringBootApplication
 public class Application {
 
