14862a7fe22afe98a525abc8be5225e050d9b4af
pchelina.a2 <pchelina.a2@wildberries.work>
2024-11-26 15:46:11 +0100

    create/upd recipe

--- content-recipe/src/main/java/org/body/balance/content/recipe/domain/food/RecipeIngredientId.java
+++ content-recipe/src/main/java/org/body/balance/content/recipe/domain/food/RecipeIngredientId.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
+@Setter
 @Getter
 @Embeddable
 @NoArgsConstructor
--- content-recipe/src/main/java/org/body/balance/content/recipe/mapper/RecipeMapper.java
+++ content-recipe/src/main/java/org/body/balance/content/recipe/mapper/RecipeMapper.java
@@ -1,11 +1,18 @@
 package org.body.balance.content.recipe.mapper;
 
-import org.body.balance.content.recipe.domain.food.Recipe;
-import org.body.balance.content.recipe.domain.food.RecipeIngredient;
-import org.body.balance.content.recipe.dto.IngredientResponseDto;
-import org.body.balance.content.recipe.dto.RecipeResponseDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.body.balance.content.recipe.domain.dictionary.Tag;
+import org.body.balance.content.recipe.domain.dictionary.Unit;
+import org.body.balance.content.recipe.domain.food.*;
+import org.body.balance.content.recipe.dto.request.IngredientRequestDto;
+import org.body.balance.content.recipe.dto.response.IngredientResponseDto;
+import org.body.balance.content.recipe.dto.request.RecipeRequestDto;
+import org.body.balance.content.recipe.dto.response.RecipeResponseDto;
+import org.mapstruct.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "spring")
 public interface RecipeMapper {
@@ -17,6 +24,45 @@ public interface RecipeMapper {
     @Mapping(source = "unit.shortName", target = "unitShortName")
     IngredientResponseDto toResponseDto(RecipeIngredient recipeIngredient);
 
+    List<RecipeResponseDto> toResponseDto(List<Recipe> recipes);
+
     RecipeResponseDto toResponseDto(Recipe recipe);
 
+    @Mapping(target = "cuisine.cuisineId", source = "cuisineId")
+    Recipe toEntity(RecipeRequestDto recipe);
+
+    default Set<Tag> mapTagsToEntity(List<Integer> tags) {
+        Set<Tag> res = new HashSet<>();
+        for (Integer id : tags) {
+            res.add(new Tag(id, null, null));
+        }
+        return res;
+    }
+
+    default List<RecipeIngredient> mapIngredientsToEntity(List<IngredientRequestDto> ingredients) {
+        List<RecipeIngredient> res = new ArrayList<>();
+
+        for (IngredientRequestDto dto : ingredients) {
+            Ingredient ingredient = new Ingredient(dto.ingredientId(), null, null);
+            Unit unit = new Unit(dto.unitId(), null, null, null);
+            RecipeIngredientId recipeIngredientId = new RecipeIngredientId(null, dto.ingredientId());
+            RecipeIngredient recipeIngredient = new RecipeIngredient(recipeIngredientId, null, ingredient, unit, dto.amount());
+            res.add(recipeIngredient);
+        }
+
+        return res;
+    }
+
+    @AfterMapping
+    default void after(@MappingTarget Recipe recipe) {
+
+        for (Step step : recipe.getSteps()) {
+            step.setRecipe(recipe);
+        }
+
+        for (RecipeIngredient ri : recipe.getIngredients()) {
+            ri.getId().setRecipeId(recipe.getRecipeId());
+            ri.setRecipe(recipe);
+        }
+    }
 }
