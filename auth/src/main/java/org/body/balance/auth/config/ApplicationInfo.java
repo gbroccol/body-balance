6d42dedac85cc012cd85f4015a5c4bfd2ec905a6
pchelina.a2 <pchelina.a2@wildberries.work>
2024-12-20 19:16:12 +0100

    monolith -> microservices

---
+++ auth/src/main/java/org/body/balance/auth/config/ApplicationInfo.java
@@ -0,0 +1,29 @@
+package org.body.balance.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationInfo implements ApplicationRunner {
+
+    @Value("${app.test.host}")
+    private String host;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${springdoc.swagger-ui.path}")
+    private String swaggerPath;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        System.out.printf("\nApplication name: %s\n", applicationName);
+        System.out.printf("Application started on host: %s:%s\n", host, port);
+        System.out.printf("Swagger: %s:%s%s\n\n", host, port, swaggerPath);
+    }
+}
